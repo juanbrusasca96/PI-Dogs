29c7afe033bb91dbe4ea43bdd107ae97dad19aa0
Juan Brusasca <juanbrusasca96@gmail.com>
2022-08-29 16:18:07 -0300

    cambios button

--- client/src/components/button/Button.jsx
+++ client/src/components/button/Button.jsx
@@ -3,10 +3,8 @@ import './button.css'
 
 export default function Button({text, onClick}) {
   return (
-    // <div className='button'>
         <button onClick={onClick} className='button'>
             {text}
         </button>
-    // {/* </div> */}
   )
 }
