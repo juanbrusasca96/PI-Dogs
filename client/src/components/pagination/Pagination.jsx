bed1ed8ebe14b7179f28a5624e153aec2d14f97e
Juan Brusasca <juanbrusasca96@gmail.com>
2022-08-29 16:24:03 -0300

    cambios pagination

--- client/src/components/pagination/Pagination.jsx
+++ client/src/components/pagination/Pagination.jsx
@@ -5,9 +5,6 @@ import './pagination.css'
 export default function Pagination({index, setPage}) {
   return (
     <div className='pagination'>
-        {/* <button onClick={()=>setPage(index-1)}>
-            {index}
-        </button> */}
         <Button text={index} onClick={()=>setPage(index-1)}/>
     </div>
   )
